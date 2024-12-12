ec215701ffdf504e46beecc0aa77cfa3ec3ea26a
thedarkking01 <sourabhsinghbais52@gmail.com>
2024-12-13 00:15:17 +0530

    Q38

---
+++ sorting and grouping/Customers Who Bought All Products.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select customer_id from
+(select customer_id,count(distinct product_key) as cnt1 from Customer  group by customer_id)a
+where a.cnt1= (select count(product_key) from Product)
